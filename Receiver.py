1eda083e5bb334011966c40d1e14f5e9a2e68593
Avi Rahimov <rahimovavi3@gmail.com>
2023-05-14 02:30:58 +0300

    Fixed the files, only one socket enabled.

--- Receiver.py
+++ Receiver.py
@@ -1,88 +1,108 @@
 import socket
 import time
+import os
+
+FIRST_ID = 3147
+SECOND_ID = 267
+# Define the IP address and port to listen on
+HOST = 'localhost'
+PORT = 12345
+# xor operation between the id's to send an authentication between the sender and the receiver
+XOR_OPERATION = str(FIRST_ID ^ SECOND_ID).encode()
+BUFFER_SIZE = 1024
+times_list_first_part = []
+times_list_second_part = []
+file_size = os.path.getsize("testing_file.txt")
+half_size = file_size // 2
+
 
 def receive_file():
     # create TCP connection and bind it to the IP address and port
-    s = socket.socket()
-    # Define the IP address and port to listen on
-    # listen on all available network interfaces
-    host = 'localhost'
-    # port number to listen on
-    port = 12345
-    first_id = 3147
-    second_id = 267
-    xor_operation = str(first_id ^ second_id).encode()
-    buffer_size = 4096
-    times_list_first_part = []
-    times_list_second_part = []
-    s.bind((host, port))
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind((HOST, PORT))
     # Listen for incoming connections
-    s.listen(1)
+    s.listen()
     # Accept a new connection
     conn, addr = s.accept()
-    # Set the congestion control algorithm to Reno for the first half
-    conn.setsockopt(socket.IPPROTO_TCP, socket.TCP_CONGESTION, b'reno')
-    # receive first part of file
-    start_time = time.time()
-    data = b""
-    print("Receiving the first half of the file from the sender")
-    while len(data) < buffer_size*buffer_size:
-        packet = conn.recv(buffer_size)
-        if not packet:
-            break
-        data += packet
-    print("Done received the first half")
-    end_time = time.time()
+    while True:
+        print(f"Connected by {addr}")
+        # Set the congestion control algorithm to Reno for the first half
+        conn.setsockopt(socket.IPPROTO_TCP, socket.TCP_CONGESTION, b'reno')
+        # receive first part of file
+        start_time = time.time()
 
-    # measure time to receive first part of file
-    first_part_time = end_time - start_time
-    times_list_first_part.append(first_part_time)
+        print("Receiving the first half of the file from the sender")
+        data = b""
+        while len(data) < half_size:
+            packet = conn.recv(1024)
+            if not packet:
+                break
+            data += packet
+        print("Done received the first half")
+        end_time = time.time()
 
-    print("Sending authentication")
-    conn.sendall(xor_operation)
+        # measure time to receive first part of file
+        first_part_time = end_time - start_time
+        times_list_first_part.append(first_part_time)
 
-    # Set the congestion control algorithm to Cubic for the second half
-    conn.setsockopt(socket.IPPROTO_TCP, socket.TCP_CONGESTION, b'cubic')
+        print("Sending authentication")
+        conn.sendall(XOR_OPERATION)
 
-    # receive second part of file
-    start_time = time.time()
-    data = b""
-    print("Receiving the second half of the file from the sender")
-    while len(data) < buffer_size*buffer_size:
-        packet = conn.recv(buffer_size)
-        if not packet:
-            break
-        data += packet
-    print("Done received the second half")
-    end_time = time.time()
+        # Set the congestion control algorithm to Cubic for the second half
+        conn.setsockopt(socket.IPPROTO_TCP, socket.TCP_CONGESTION, b'cubic')
 
-    # measure time to receive second part of file
-    second_part_time = end_time - start_time
-    times_list_second_part.append(second_part_time)
-
-    print("Waiting for the sender to send more files or to close the connection...")
-    # check if sender wants to send file again or exit
-    msg = conn.recv(buffer_size).decode()
-    if msg == "Send Again":
-        print("Receiving more files!")
+        # receive second part of file
+        start_time = time.time()
+        print("Receiving the second half of the file from the sender")
+        data = b""
+        while len(data) < half_size:
+            packet = conn.recv(1024)
+            if not packet:
+                break
+            data += packet
+        print("Done receiving the second half")
+        # conn.sendall(XOR_OPERATION)
+        end_time = time.time()
         print("-----------------------------------------------------------------------------------")
-        receive_file()
-    elif msg == "Exit":
-        print("-----------------------------------------------------------------------------------")
-        print("Calculating times: ")
-        # calculate average time
-        avg_first_part_time = first_part_time
-        avg_second_part_time = second_part_time
-        total_time = avg_first_part_time + avg_second_part_time
-        print("Average time to receive first part of file: ", avg_first_part_time)
-        print("Average time to receive second part of file: ", avg_second_part_time)
-        print("Total time taken to receive file: ", total_time)
-        # close TCP connection
-        conn.sendall("Goodbye".encode())
-        conn.close()
-    else:
-        # if message is not recognized, close TCP connection
-        conn.sendall("Invalid Message".encode())
-        conn.close()
+        # measure time to receive second part of file
+        second_part_time = end_time - start_time
+        times_list_second_part.append(second_part_time)
 
-        receive_file()
+        print("Waiting for the sender to send more files or to close the connection...")
+        # check if sender wants to send file again or exit
+        conn.sendall("waiting".encode())
+
+        msg = conn.recv(1).decode()
+        if msg == "y":
+            print("Receiving more files!")
+            print("-----------------------------------------------------------------------------------")
+            # receive_file()
+            continue
+        elif msg == "n":
+            print("-----------------------------------------------------------------------------------")
+            print("Calculating times: ")
+            # Calculate the total time taken to receive the file
+            avg_time_first_part = sum(times_list_first_part) / len(times_list_first_part)
+            avg_time_second_part = sum(times_list_second_part) / len(times_list_second_part)
+            # printing the times without average
+            print("Sending times for RENO CC algorithm: ")
+            for i in range(len(times_list_first_part)):
+                print(f"Iteration number: {i + 1}, time: {times_list_first_part[i]}")
+            print("-----------------------------------------------------------------------------------")
+            print("Sending times for CUBIC CC algorithm: ")
+            for i in range(len(times_list_first_part)):
+                print(f"Iteration number: {i + 1}, time: {times_list_second_part[i]}")
+            print("-----------------------------------------------------------------------------------")
+            # Print the time taken to receive the file
+            print(f"first half files by RENO algorithm received in average time of {avg_time_first_part} seconds")
+            print(f"second half files by CUBIC algorithm received in average time of {avg_time_second_part} seconds")
+            break
+        else:
+            print("Invalid Message occurred")
+            # if message is not recognized, close TCP connection
+            conn.sendall("Invalid Message".encode())
+            conn.close()
+            break
+
+
+receive_file()
--- Sender.py
+++ Sender.py
@@ -1,50 +1,69 @@
 import socket
-import time
+
+FIRST_ID = 3147
+SECOND_ID = 267
+HOST = 'localhost'
+PORT = 12345
+# xor operation between the id's to send an authentication between the sender and the receiver
+XOR_OPERATION = str(FIRST_ID ^ SECOND_ID)
+
+
+def splitting_data(file_name) -> tuple:
+    with open(file_name, "rb") as file:
+        file_data = file.read()
+    # Split the file into two parts (first half and second half)
+    file_size = len(file_data)
+    half_size = file_size // 2
+    first_half = file_data[:half_size]
+    second_half = file_data[half_size:]
+    return first_half, second_half
 
 
 def send_file():
     file_name = 'testing_file.txt'
-    with open(file_name, "rb") as file:
-        file_data = file.read()
+    first_half, second_half = splitting_data(file_name)
 
     # create TCP connection
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    first_id = 3147
-    second_id = 267
-    # xor operation between the id's to send an authentication between the sender and the receiver
-    xor_operation = str(first_id ^ second_id)
-    host = "127.0.0.1"
-    port = 12345
-    s.connect((host, port))
-
-    # Send the first half of the file with Reno algorithm and after that closing the socket
-    s.setsockopt(socket.IPPROTO_TCP, socket.TCP_CONGESTION, b'reno')
-    # send first part of file
-    s.sendall(file_data[:len(file_data) // 2])
-
-    # receive authentication response
-    auth_response = s.recv(1024).decode()
-    if auth_response == xor_operation:
-        print("Authentication Successful")
-    else:
-        print("Authentication Failed")
-        return
-
-    # Send the second half of the file with Cubic algorithm and after that closing the socket
-    s.setsockopt(socket.IPPROTO_TCP, socket.TCP_CONGESTION, b'cubic')
-    # send second part of file
-    s.sendall(file_data[len(file_data) // 2:])
-
-    # prompt user to send file again
-    send_again = input("Send file again? (y/n): ")
-    if send_again.lower() == "y":
-        s.sendall("Send Again".encode())
-        # change back the congestion control algorithm
-        # ...
-        send_file()
-    else:
-        s.sendall("Exit".encode())
-        s.close()
+    s.connect((HOST, PORT))
+    print(f"Connected to {HOST} with port {PORT}")
+    while True:
+        # Send the first half of the file with Reno algorithm and after that closing the socket
+        print("Setting the socket to reno CC algorithm")
+        s.setsockopt(socket.IPPROTO_TCP, socket.TCP_CONGESTION, b'reno')
+        # send first part of file
+        print("Sending the first part of the file")
+        s.sendall(first_half)
+        print("finish to send the first part of the file")
+        print("-----------------------------------------------------------------------------------")
+        # receive authentication response
+        print("Waiting for authentication message...")
+        auth_response = s.recv(1024).decode()
+        if auth_response == XOR_OPERATION:
+            print("Authentication Successful")
+        else:
+            print("Authentication Failed")
+            return
+        print("-----------------------------------------------------------------------------------")
+        # Send the second half of the file with Cubic algorithm and after that closing the socket
+        print("Setting the socket to cubic CC algorithm")
+        s.setsockopt(socket.IPPROTO_TCP, socket.TCP_CONGESTION, b'cubic')
+        # send second part of file
+        print("Sending the second part of the file")
+        s.sendall(second_half)
+        print("finish to send the second part of the file")
+        print("-----------------------------------------------------------------------------------")
+        # prompt user to send file again
+        receiver_msg = s.recv(7).decode()
+        if receiver_msg == "waiting":
+            send_again = input("Do you want to send file again? (y/n): ")
+            if send_again.lower() == "y":
+                s.sendall("y".encode())
+                # send_file()
+            else:
+                s.sendall("n".encode())
+                s.close()
+                break
 
 
 send_file()
